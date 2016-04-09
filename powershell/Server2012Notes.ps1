43606f774fac1bad0c7fe5d5aa0d7ffb1bd434df
Bradley Gillap <bradgillap@gmail.com>
2016-04-09 01:10:43 -0400

    Update Server2012Notes.ps1
    Formatting

--- powershell/Server2012Notes.ps1
+++ powershell/Server2012Notes.ps1
@@ -5,14 +5,14 @@ D:\
 .\VBoxWinddowsAdditions.exe
 ------------------------------------------------------------------------
 -- SERVICES COMMANDS
-Get-Service		--Get information about a service
-Start-Service	--Start a service
-Stop-Service	--Stop a service
+Get-Service		  --Get information about a service
+Start-Service	  --Start a service
+Stop-Service	  --Stop a service
 Restart-Service --Restart a service
 Suspend-Service --Pause a service
 Resume-Service	--Resume suspended service
-Set-Service		--Changes properties of a service
-New-Service		--Create a service
+Set-Service		  --Changes properties of a service
+New-Service		  --Create a service
 ------------------------------------------------------------------------
 -- SHOW INSTALLED, REMOVED, AVAILABLE
 Get-WindowsFeature | where InstallState -eq Installed	--Show things that are installed
@@ -24,16 +24,16 @@ Get-WindowsFeature | where InstallState -eq Available	--Show possible things to 
 Get-Something
 Set-Something
 ------------------------------------------------------------------------
-Get-Help							-- Downloads powershell help for switches on whatever commands
+Get-Help							              -- Downloads powershell help for switches on whatever commands
 Get-Help WindowsFeature -examples   -- Literally shows examples of the command
-Get-Help WindowsFeature -detailed	-- Detailed description condensed
-Get-Help WindowsFeature -Full		-- Full description
-Get-Help WindowsFeature -Online		-- Microsoft website description
+Get-Help WindowsFeature -detailed	  -- Detailed description condensed
+Get-Help WindowsFeature -Full		    -- Full description
+Get-Help WindowsFeature -Online		  -- Microsoft website description
 
-Add-WindowsFeature					-- Add windows features 
+Add-WindowsFeature					  -- Add windows features 
 Uninstall-WindowsFeature			-- Remove windows features
 
-sconfig								-- Configure server powershell page
+sconfig								        -- Configure server powershell page
 ------------------------------------------------------------------------
 -- Turn on Powershell for servercore remoting
 Enable-PSRemoting -Force
@@ -58,45 +58,45 @@ New-NetFirewallRule –DisplayName "Allow Ping" –Direction Inbound –Action A
 Get-Service				-- Display services use "-DisplayName Hy" to search services
 Start-Service			-- Start a service name, use a comma to seperate more than one.
 Stop-Service			-- Stop service
-Restart-Service			-- Restart service
-Suspend-Service			-- Pause service
-Resume-Service			-- Resume paused service
+Restart-Service		-- Restart service
+Suspend-Service		-- Pause service
+Resume-Service		-- Resume paused service
 Set-Service				-- Change properties of a service
 New-Service				-- Creats a new service
 
 ------------------------------------------------------------------------
 -- NICTEAM CONTROL
 
-Get-NetLbfoTeam			-- Shows a list of NIC teams on the server
-New-NetLbfoTeam			-- Createse a NIC team and adds network adapters to it. You can optionally set the teams properties
-Remove-NetLbfoTeam		-- Delete a team
-rename-NetLbfoTeaem		-- Rename a team
-Set-NetLbfoTeaem		-- Sets the properties
+Get-NetLbfoTeam			  -- Shows a list of NIC teams on the server
+New-NetLbfoTeam			  -- Createse a NIC team and adds network adapters to it. You can optionally set the teams properties
+Remove-NetLbfoTeam	  -- Delete a team
+rename-NetLbfoTeaem	  -- Rename a team
+Set-NetLbfoTeaem		  -- Sets the properties
 
 ------------------------------------------------------------------------
 -- CREATE SAMBA SHARES
 
-New-SmbShare			-- Create Share
-Get-SmbShare			-- Lists shares on the computer
-Remove-SmbShare			-- Delete SmbShare
-Set-SmbShare			-- Changse a share's properties
+New-SmbShare			    -- Create Share
+Get-SmbShare			    -- Lists shares on the computer
+Remove-SmbShare		    -- Delete SmbShare
+Set-SmbShare			    -- Changse a share's properties
 Get-SmbShareAccess		-- Displays permission for a share
 Grant-SmbShareAccess	-- Adds a permission to a share
 
 ------------------------------------------------------------------------
 -- CREATE DNS ZONES
-Add-DnsServerPrimaryZone csmtech.local -ReplicationScope Domain						--AD Domain wide forward lookup zoneFLZ
-Add-DnsServerPrimaryZone csmtech.local -ZoneFile csmtech.local.dns					--Creates standard FLZ
-Add-DnsServerPrimaryZone -NetworkID 10.10.0.0/16 -ReplicationScope Forest			--AD Reverse Lookup ZoneRLZ
+Add-DnsServerPrimaryZone csmtech.local -ReplicationScope Domain						        --AD Domain wide forward lookup zoneFLZ
+Add-DnsServerPrimaryZone csmtech.local -ZoneFile csmtech.local.dns					      --Creates standard FLZ
+Add-DnsServerPrimaryZone -NetworkID 10.10.0.0/16 -ReplicationScope Forest		      --AD Reverse Lookup ZoneRLZ
 Add-DnsServerPrimaryZone -NetworkID 10.10.0.0/16 -ZoneFile 10.10.in-addr.arpa.dns	--Standard RLZ
-Add-DnsServerSecondaryZone															--Create secondary dns server Aut
-Add-DnsServerStbZone																--Create stub zone dns server
-Add-DnsServerForwarder -IPAddress 10.10.1.2											--Adds forwarders to DNS server forward list
-Set-DnsServerForwarder -IPAddress 10.10.1.3											--Changes forwarder address
-Add-DnsServerRootHint root.mydomain.local -IPAddress 10.10.1.50						--Adds root hint to DNS server
-Import-DnsServerRootHint 410server.testdom1.local									--Imports root hints from another dns server
-Set-DnsServerRecursion -Enable $true												--Sets recursion settings
-Set-DnsServerDiagnostics -All $true													--Turns on debugging and logging parameters
+Add-DnsServerSecondaryZone															                          --Create secondary dns server Aut
+Add-DnsServerStbZone																                              --Create stub zone dns server
+Add-DnsServerForwarder -IPAddress 10.10.1.2											                  --Adds forwarders to DNS server forward list
+Set-DnsServerForwarder -IPAddress 10.10.1.3											                  --Changes forwarder address
+Add-DnsServerRootHint root.mydomain.local -IPAddress 10.10.1.50						        --Adds root hint to DNS server
+Import-DnsServerRootHint 410server.testdom1.local									                --Imports root hints from another dns server
+Set-DnsServerRecursion -Enable $true												                      --Sets recursion settings
+Set-DnsServerDiagnostics -All $true													                      --Turns on debugging and logging parameters
 ------------------------------------------------------------------------
 -- INSTALL A.D IN SERVER CORE YOU POOR BASTARD (NO GUI TOOLS).
 
@@ -104,8 +104,8 @@ Set-DnsServerDiagnostics -All $true													--Turns on debugging and logging
 Add-WindowsFeature AD-Domain-Services
 -- PROMOTION
 Install-ADDSDomainController -DomainName "410server2012.local"		-- Install DC to existing domain
-Install-ADDSDomain -DomainName "410Server2012.local"				-- New domain to existing forest or new whatever.com address
-Install-ADDSForest -DomainName "410Server2012.local"				- New DC in a NEW forest
+Install-ADDSDomain -DomainName "410Server2012.local"				      -- New domain to existing forest or new whatever.com address
+Install-ADDSForest -DomainName "410Server2012.local"				      -- New DC in a NEW forest
 
 ------------------------------------------------------------------------
 -- HYPER-V
