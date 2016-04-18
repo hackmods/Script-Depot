ed232fdf100021c0af18715e0429f2b7eca50818
Ryan Morris <saveourmacs@gmail.com>
2016-04-18 18:34:45 -0400

    InitializeGit bash
    InitializeGit.sh bash file

---
+++ sh/InitializeGit.sh
@@ -0,0 +1,5 @@
+#set up git and initialize a project
+sudo apt-get install git
+git config --global user.name "SampleName"
+git config --global user.email "SName@email.com "
+git init && git add -A && git commit -m "Initializing Project"
