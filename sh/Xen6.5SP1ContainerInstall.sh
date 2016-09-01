9e878dfe6826d2d3f6f9e30bbd714a63034041d2
Ryan Morris <saveourmacs@gmail.com>
2016-08-31 20:33:17 -0400

    Configure containers for XenServer 6.5 SP 1

---
+++ sh/Xen6.5SP1ContainerInstall.sh
@@ -0,0 +1,4 @@
+# Get and install docker [container] support for XenServer 7
+$ wget http://downloadns.citrix.com.edgesuite.net/10343/XenServer-6.5.0-SP1-xscontainer.iso
+$ xe-install-supplemental-pack XenServer-6.5.0-SP1-xscontainer.iso
+ 
