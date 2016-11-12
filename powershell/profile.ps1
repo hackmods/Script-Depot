6703227b6c2c6b1830a772c7f3e4dbc6cd978620
Bradley Gillap <bradgillap@gmail.com>
2016-11-12 02:04:07 -0500

    User profile functions
    A series of useful commands to load every time.

---
+++ powershell/profile.ps1
@@ -0,0 +1,47 @@
+<#
+
+.SYNOPSIS
+This is a profile powershell script of functions to be loaded with the shell environment.
+
+.DESCRIPTION
+The script will run various functions outlined every time the user opens the shell.
+
+.EXAMPLE
+Get-DriveSizes
+
+.NOTES
+Drop the this in your %USERPROFILE%/Documents/WindowsPowerShell/profile.ps1
+
+.LINK
+http://www.bradgillap.com
+
+#>
+
+$functionNames = "`nGet-OsInfo","`nGet-BIOSInfo","`nGet-DriveSizes"
+
+
+Write-Host -ForegroundColor yellow "Welcome, the following commands may also be used"
+Write-Host -ForegroundColor green "$functionNames"
+
+function Get-OSInfo {
+param (
+    $computerName = 'localhost'
+)
+get-wmiobject -class win32_operatingsystem -Computer $computername | Select-Object BuildNumber,Caption,ServicePackMajorVersion
+
+}
+
+function Get-BIOSInfo {
+param (
+    $computerName = 'localhost'
+)
+get-wmiobject -class win32_bios -computer $computername
+
+}
+
+function Get-DriveSize {
+
+Get-WMIObject -class Win32_LogicalDisk -computer $env:COMPUTERNAME | 
+Select-Object DeviceID, @{Name="Size (GB)";Expression={"{0:N2}" -f ($_.Size / 1GB) }},@{Name="FreeSpace (GB)";Expression={"{0:N2}" -f ($_.freespace / 1GB) }}
+
+}
